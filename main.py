97c6601c2a157799756cf95bc2360d79e67bcc5b
isaacakpasu006 <isaac.akpasu@binhamuni.edu.ng>
2023-11-08 19:03:06 +0100

    removed something

--- main.py
+++ main.py
@@ -13,4 +13,3 @@ compound_intrest = initial_principal * (1+(interest_rate/number_of_time_period_e
 print("YUSUF AND SONS COMPANY")
 print(f'Simple intrest :  {simple_intrest}')
 print(f'Compound intrest : {compound_intrest}')
-#added something to my8 main
